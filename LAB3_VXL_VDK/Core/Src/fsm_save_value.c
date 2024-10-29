8293c92b08a1f3b04ba32008b30937e15d00c12a
LAPTOP-T5UI490L\Admin <huynh.nguyenvank22@hcmut.edu.vn>
2024-10-29 23:44:58 +0700

    update fsm_save_value 2nd

--- LAB3_VXL_VDK/Core/Src/fsm_save_value.c
+++ LAB3_VXL_VDK/Core/Src/fsm_save_value.c
@@ -10,18 +10,24 @@ void fsm_save_value_run() {
 	switch (status) {
 	case RED_SAVE:
 		time_red = time_red;
+		clearLed();
+		clearLed1();
 		if (timer1_flag == 1) {
 			status = INIT;
 		}
 		break;
 	case GREEN_SAVE:
 		time_green = time_green;
+		clearLed();
+		clearLed1();
 		if (timer1_flag == 1) {
 			status = INIT;
 		}
 		break;
 	case YELLOW_SAVE:
 		time_yellow = time_yellow;
+		clearLed();
+		clearLed1();
 		if (timer1_flag == 1) {
 			status = INIT;
 		}
